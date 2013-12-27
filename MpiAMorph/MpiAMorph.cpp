11426e965fe63cc81de39a1426fd69fd88348cbb
Yuriy Zhayvoronok <yuzh@studunion.ru>
2013-12-27 09:00:49 +0400

    tags

--- MpiAMorph/MpiAMorph.cpp
+++ MpiAMorph/MpiAMorph.cpp
@@ -88,7 +88,6 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-
 bool FileExists(char *filename)
 {
 	bool exists = false;
@@ -111,9 +110,14 @@ void MasterProcess()
 	unsigned char * dst_vector;
 
 	cout << "Total nodes: " << totalNodes << endl;
+
+	//Синхронизация процессов
 	MPI_Barrier(MPI_COMM_WORLD);
 
+	//Загрузка исходного изображения
 	load(srcFile, &src_vector, &sizeMtx, &sizeCell);
+
+	//Загрузка конечного изображения
 	load(dstFile, &dst_vector, &sizeMtx, &sizeCell);
 
 	for (int i = 0; i < sizeMtx; i++)
@@ -130,6 +134,14 @@ void MasterProcess()
 	//Раздать вершины подграфов по узлам
 	for (int i = 0; i < totalNodes && i < sizeMtx; i++)
 	{
+		//MPI_Send:
+		//buf	 -	 адрес начала расположения пересылаемых данных;
+		//count	 -	 число пересылаемых элементов;
+		//datatype	 -	 тип посылаемых элементов;
+		//dest	 -	 номер процесса-получателя в группе, связанной с коммуникатором comm;
+		//tag	 -	 идентификатор сообщения (аналог типа сообщения функций nread и nwrite PSE nCUBE2);
+		//comm	 -	 коммуникатор области связи.
+
 		MPI_Send((void *)q.front(),2,MPI_LONG,i+1,1,MPI_COMM_WORLD);
 		q.pop();
 	}
@@ -140,10 +152,13 @@ void MasterProcess()
 		//MPI_Recv(&i,1, MPI_INT,0,1,MPI_COMM_WORLD,&status);
 	}
 
+
+	//Освобождение памяти
 	free(src_vector);
 	free(dst_vector);
 }
 
+//Исполняющий процесс
 void SlaveProcess()
 {
 	int i = 0;
@@ -154,10 +169,15 @@ void SlaveProcess()
 	unsigned char * src_vector;
 	unsigned char * dst_vector;
 
+	//Синхронизация процессов
 	MPI_Barrier(MPI_COMM_WORLD);
+
 	cout << "Current node: " << myNode << endl;
 
+	//Загрузка исходного изображения
 	load(srcFile, &src_vector, &sizeMtx, &sizeCell);
+
+	//Загрузка конечного изображения
 	load(dstFile, &dst_vector, &sizeMtx, &sizeCell);
 
 	//for(;;)
@@ -165,10 +185,26 @@ void SlaveProcess()
 		int error,count ;
 		unsigned long* buffer;
 		MPI_Status status ;
+
+		//Позволяет проверить входные сообщения без их реального приема
 		error = MPI_Probe(0,1,MPI_COMM_WORLD,&status);
+
+		//Получить длину принимаемого сообщения
 		error = MPI_Get_count(&status,MPI_LONG,&count);
+
+		//Выделить память
 		buffer = (unsigned long*)malloc (sizeof(long)*count) ;
+
 		if ( buffer == NULL ){};
+
+		//MPI_Recv:
+		//OUT	 buf	 -	 адрес начала расположения принимаемого сообщения;
+		//IN	 count	 -	 максимальное число принимаемых элементов;
+		//IN	 datatype	 -	 тип элементов принимаемого сообщения;
+		//IN	 source	 -	 номер процесса-отправителя;
+		//IN	 tag	 -	 идентификатор сообщения;
+		//IN	 comm	 -	 коммуникатор области связи;
+		//OUT	 status	 -	 атрибуты принятого сообщения.
 		error = MPI_Recv (buffer,count,MPI_LONG,0,1,MPI_COMM_WORLD,&status);
 
 		for (int i = 0; i < count; i++)
@@ -176,6 +212,8 @@ void SlaveProcess()
 			cout<<buffer[i]<< " ";
 		}
 		cout<< endl;
+
+		//ToDo: Отправка на обход дерева по вектору
 
 		free(buffer);
 	}
